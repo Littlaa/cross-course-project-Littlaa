cde16b7d39b82841b7c484c9473964e7517de671
Littlaa <monadags@gmail.com>
2022-03-16 21:20:27 +0100

    Added some JS to contact

---
+++ js/contact.js
@@ -0,0 +1,36 @@
+const contactForm = document.querySelector("#contactForm");
+const fullName = document.querySelector("#name");
+const nameError = document.querySelector("#nameError");
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#emailError");
+
+function formValidation(event) {
+  event.preventDefault();
+
+  if (minLength(fullName.value, 0)) {
+    nameError.style.display = "none";
+  } else {
+    nameError.style.display = "block";
+  }
+  if (emailValidation(email.value)) {
+    emailError.style.display = "none";
+  } else {
+    emailError.style.display = "block";
+  }
+}
+
+contactForm.addEventListener("submit", formValidation);
+
+function minLength(value, len) {
+  if (value.trim().length > len) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function emailValidation(email) {
+  const regEx = /\S+@\S+\.\S+/;
+  const emailMatch = regEx.test(email);
+  return emailMatch;
+}
