7eaf24912ff527c2f0a73308a15f9cd9524278a5
Littlaa <monadags@gmail.com>
2022-04-23 01:15:26 +0200

    Update list.js

--- js/list.js
+++ js/list.js
@@ -1,4 +1,5 @@
-const baseUrl = "https://littlaa.one/rainydays/wp-json/wc/v3/products";
+const baseUrl =
+  "https://littlaa.one/rainydays/wp-json/wc/store/products?consumer_key=ck_f439517697d4080770ef3129d79e40541faa4ef&consumer_secret=cs_9f4811b94a92e24c9e6064f6d9fc886425585fdf";
 
 async function getProducts(url) {
   const response = await fetch(url);
